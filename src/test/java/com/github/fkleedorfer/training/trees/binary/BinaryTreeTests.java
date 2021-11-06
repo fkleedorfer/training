538fa3fc16a9cf368f2333fa754e98a4718838dc
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2021-11-06 18:17:05 +0100

    Improve tests

--- src/test/java/com/github/fkleedorfer/training/trees/binary/BinaryTreeTests.java
+++ src/test/java/com/github/fkleedorfer/training/trees/binary/BinaryTreeTests.java
@@ -4,8 +4,62 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BinaryTreeTests {
+
     @Test
-    public void testAdd(){
+    public void testSize(){
+        BinaryTree<Integer> tree = prepareTree();
+        Assertions.assertEquals(7,tree.size());
+    }
+
+    @Test
+    public void testSize_emptyTree(){
+        BinaryTree<Integer> tree = prepareEmptyTree();
+        Assertions.assertEquals(0,tree.size());
+    }
+
+    @Test
+    public void testMin(){
+        BinaryTree<Integer> tree = prepareTree();
+        Assertions.assertEquals(2, tree.min());
+    }
+
+    @Test
+    public void testMin_emptyTree(){
+        BinaryTree<Integer> tree = prepareEmptyTree();
+        Assertions.assertEquals(null,tree.min());
+    }
+
+    @Test
+    public void testMax(){
+        BinaryTree<Integer> tree = prepareTree();
+        Assertions.assertEquals(11, tree.max());
+    }
+
+    @Test
+    public void testMax_emptyTree(){
+        BinaryTree<Integer> tree = prepareEmptyTree();
+        Assertions.assertEquals(null,tree.max());
+    }
+
+    @Test
+    public void testContains(){
+        BinaryTree<Integer> tree = prepareTree();
+        Assertions.assertTrue(tree.contains(2));
+        Assertions.assertTrue(tree.contains(10));
+        Assertions.assertFalse(tree.contains(1));
+        Assertions.assertFalse(tree.contains(20));
+    }
+
+    @Test
+    public void testContains_emptyTree(){
+        BinaryTree<Integer> tree = prepareEmptyTree();
+        Assertions.assertFalse(tree.contains(2));
+        Assertions.assertFalse(tree.contains(10));
+        Assertions.assertFalse(tree.contains(1));
+        Assertions.assertFalse(tree.contains(20));
+    }
+
+    private BinaryTree<Integer> prepareTree() {
         BinaryTree<Integer> tree = new BinaryTree<Integer>();
         tree.add(5);
         tree.add(2);
@@ -14,12 +68,10 @@ public class BinaryTreeTests {
         tree.add(8);
         tree.add(9);
         tree.add(11);
-        Assertions.assertEquals(7,tree.size());
-        Assertions.assertEquals(2, tree.min());
-        Assertions.assertEquals(11, tree.max());
-        Assertions.assertTrue(tree.contains(2));
-        Assertions.assertTrue(tree.contains(10));
-        Assertions.assertFalse(tree.contains(1));
-        Assertions.assertFalse(tree.contains(20));
+        return tree;
+    }
+
+    private BinaryTree<Integer> prepareEmptyTree() {
+        return new BinaryTree<Integer>();
     }
 }
